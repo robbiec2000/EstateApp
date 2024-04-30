e3a0c512037466f9e0507dbc5d181307c140f8e1
Robbie <siboc@student.unimelb.edu.au>
2024-04-30 19:59:41 +1000

    login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const register = async (req, res) => {
     });
     res.status(201).json({ message: "user create success" });
   } catch {
-    res.status(500).json({ message: "user create fail" });
+    res.status(500).json({ message: "Fail to create user" });
   }
 };
 
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "invalid credentials" });
+      return res.status(401).json({ message: "Incorrect username or password" });
     }
 
     const age = 1000 * 60 * 60 * 24 * 7;
@@ -40,13 +40,15 @@ export const login = async (req, res) => {
       expiresIn: age,
     });
 
+    const {password: userPassword, ...userInfo} = user;
+
     res
       .cookie("token", token, {
         httpOnly: true,
         maxAge: age,
       })
       .status(200)
-      .json({ message: "login success" });
+      .json(userInfo);
   } catch {
     res.status(500).json({ message: "login fail" });
   }
